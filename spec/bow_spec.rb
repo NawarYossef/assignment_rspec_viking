40a907aa75b5cc4c5e7548fbb2d4f5d80ffb28a0
Nawar <nawaryossef2@gmail.com>
2017-04-20 19:54:47 -0500

    finish test bow

--- spec/bow_spec.rb
+++ spec/bow_spec.rb
@@ -1,1 +1,24 @@
-# Your code here
+require'./lib/weapons/bow'
+
+describe Bow do
+  let(:bow) {Bow.new}
+  let(:bow_8_arrows) {Bow.new(8)}
+
+  describe '#initialize' do
+    it "ensures that arrow count is set to 10 by default" do
+      expect(bow.arrows).to eq(10)
+    end
+    it "returns the value passed upon instantiation as the default arrow count" do
+      expect(bow_8_arrows.arrows).to eq(8)
+    end
+  end
+
+  describe '#use' do
+    it 'returns an error if the number of arrows is less than 1' do
+      expect{ Bow.new(0).use }.to raise_error('Out of arrows')
+    end
+    it "decrement the number of arrows by 1" do
+      expect{ bow_8_arrows.use }.to change(bow_8_arrows, :arrows).from(8).to(7)
+    end
+  end
+end
---
+++ spec/warmup_spec.rb
@@ -0,0 +1,39 @@
+require 'warmup'
+
+describe Warmup do
+
+ let(:user) { Warmup.new }
+
+   describe '#gets_shout' do
+    it "return string in upcase" do
+ 	    allow(user).to receive(:gets).and_return('value')
+ 	    expect(user.gets_shout).to eq("VALUE")
+     end
+   end
+
+   describe "#triple_size" do
+     it "returns triple the size of the array object" do
+       array_double = instance_double('arr', size: 7)
+       expect(user.triple_size(array_double)).to eq(21)
+     end
+    describe "#calls_some_methods" do
+      # string_double = instance_double('string')
+
+      it "raises an error if string passed is empty" do
+        empty_string = ''
+        expect { Warmup.new.calls_some_methods(empty_string) }.to raise_error("Hey, give me a string!")
+      end
+
+      it "calls the upcase! method on the string" do
+        string = "value"
+        expect(string.upcase!).to eq("VALUE")
+      end
+
+      it "calls the reverse! method on the string" do
+        string = "value"
+        expect(string.reverse!).to eq("eulav")
+      end
+
+    end
+  end
+end
